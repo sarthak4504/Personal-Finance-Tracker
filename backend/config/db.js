51db7181459470c424cf1dd8713e6f9c11b3b445
sarthak4504 <sarthak.pathak4504@gmail.com>
2025-07-15 09:41:26 +0530

    fixed mongo db connection issues

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,14 @@
-const moongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await moongoose.connect(process.env.MONGO_URI, {});
-        console.log('MongoDB connected');
+        console.log("Connecting to MongoDB...");
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
+
     } catch (err) {
         console.error('Error connecting to MongoDB', err);
-        process.exit(1); 
+        process.exit(1);
     }
 }
 
