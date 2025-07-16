5e315439f094dccf0b2fe701fb6b1d63994b0811
sarthak4504 <sarthak.pathak4504@gmail.com>
2025-07-16 14:02:18 +0530

    created authRoutes, authController and initialzed user file

---
+++ backend/controllers/authController.js
@@ -0,0 +1,51 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+// Generate JWT token 
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
+
+// Register user
+exports.registerUser = async (req, res) => {
+    const { fullName, email, password, profileImageUrl } = req.body;
+
+    //Validation: check for missing fields
+    if (!fullName || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    try {
+        // Check if email already exists
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "Email already in use" });
+        }
+
+        // Create new user
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl,
+        });
+
+        res.status(201).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    } catch (error) {
+        res
+            .status(500)
+            .json({ message: "Error registering user", error: error.message });
+    }
+
+};
+
+// Login user
+exports.loginUser = async (req, res) => { };
+
+// Get user info
+exports.getUserInfo = async (req, res) => { };
