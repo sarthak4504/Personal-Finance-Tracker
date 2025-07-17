b726d0e84b5011e6f1adb8a0cd8fe9dd7efd85ba
sarthak4504 <sarthak.pathak4504@gmail.com>
2025-07-17 18:21:15 +0530

    Created Authentication APIs (Login, Sign-Up, Get User Info, Profile Image Upload)

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -45,7 +45,42 @@ exports.registerUser = async (req, res) => {
 };
 
 // Login user
-exports.loginUser = async (req, res) => { };
+exports.loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    //Validation: check for missing fields
+    if (!email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(401).json({ message: "Invalid credentials" });
+        }
+
+        res.status(200).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    } catch (error) {
+        res
+            .status(500)
+            .json({ message: "Error logging in", error: error.message });
+    }
+};
 
 // Get user info
-exports.getUserInfo = async (req, res) => { };
+exports.getUserInfo = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: "Error fetching user info", error: error.message });
+    }
+};
