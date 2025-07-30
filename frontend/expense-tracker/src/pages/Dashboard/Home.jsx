6a7b188a82d68456bdc983ce36f58bd66a96a7a2
sarthak4504 <sarthak.pathak4504@gmail.com>
2025-07-30 15:39:29 +0530

    Created Recent Transactions Section

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -8,6 +8,7 @@ import InfoCard from '../../components/Cards/InfoCard';
 import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu';
 import { IoMdCard } from 'react-icons/io';
 import { addThousandsSeparator } from '../../utils/helper';
+import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 
 const Home = () => {
   useUserAuth();
@@ -66,6 +67,14 @@ const Home = () => {
             color="bg-red-500"
           />
         </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-6'>
+          <RecentTransactions
+            transactions={dashboardData?.recentTransactions}
+            onSeeMore={() => navigate("/expense")}
+          />
+        </div>
+
       </div>
     </DashboardLayout>
   );
